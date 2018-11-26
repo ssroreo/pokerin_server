65aa50be6dab637886707d9fbbc663a7ff3af122
ssroreo <527191915@qq.com>
2018-11-26 22:02:28 +0800

    Update login.py

--- login.py
+++ login.py
@@ -2,14 +2,31 @@
 # -*- coding: UTF-8 -*-
 
 # CGI处理模块
-import cgi, cgitb 
+import cgi, cgitb, MySQLdb
 
 # 创建 FieldStorage 的实例化
 form = cgi.FieldStorage() 
 
 # 获取数据
 site_name = form.getvalue('logname')
-site_url  = form.getvalue('logpass')
+site_pass  = form.getvalue('logpass')
+
+db = MySQLdb.connect("localhost","root","123456","PokerinDB",charset="utf-8")
+
+cursor = db.cursor()
+
+sql="select userPass from UserInfo where userName='%s'" % (site_name)
+
+cursor.execute(sql)
+
+userPass = cursor.fetchone()
+
+if userPass == site_pass:
+    result="success"
+else:
+    result="fail"
+
+db.close()
 
 print "Content-type:text/html"
 print
@@ -19,6 +36,6 @@ print "<meta charset=\"utf-8\">"
 print "<title>菜鸟教程 CGI 测试实例</title>"
 print "</head>"
 print "<body>"
-print "<h2>%s官网：%s</h2>" % (site_name, site_url)
+print "<h2>%s%s</h2>" % (result)
 print "</body>"
 print "</html>"
