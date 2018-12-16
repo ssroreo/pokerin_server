b732189c894eb147494bdcb725e33afccd636a30
ssroreo <527191915@qq.com>
2018-12-16 12:24:39 +0800

    Update login.py

--- login.py
+++ login.py
@@ -16,7 +16,7 @@ db = MySQLdb.connect("localhost", "root", "123456", "PokerinDB", charset='utf8' 
 
 # 使用cursor()方法获取操作游标 
 cursor = db.cursor()
-results = ""
+userPass = ""
 # SQL 查询语句
 sql = "SELECT userPass FROM UserInfo where userName='%s'" % (site_name)
 try:
@@ -24,9 +24,10 @@ try:
    cursor.execute(sql)
    # 获取所有记录列表
    results = cursor.fetchall()
-   if results != "":
-    for row in results:
-        userPass = row[0]
+   if results.hasNext():
+       for row in results:
+           userPass = row[0]
+
 except:
    userPass = ""
 
